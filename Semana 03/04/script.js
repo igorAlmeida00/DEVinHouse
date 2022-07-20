9e9574b77499d26d4e9c9693d6de68d5b08e4155
Igor Almeida <96877821+igorAlmeida00@users.noreply.github.com>
2022-07-20 00:19:33 -0300

    Add files via upload

---
+++ Semana 03/04/script.js
@@ -0,0 +1,15 @@
+const titulo1 = document.getElementsByClassName("text1")[0];
+titulo1.innerText = "PRIMEIRO";
+titulo1.style.fontSize = "2rem";
+
+const titulo2 = document.getElementsByClassName("text2")[0];
+titulo2.innerText = "SEGUNDO";
+titulo2.style.fontSize = "2rem";
+
+const titulo3 = document.getElementsByClassName("text3")[0];
+titulo3.innerText = "TERCEIRO";
+titulo3.style.fontSize = "2rem";
+
+const titulo4 = document.getElementsByClassName("text4")[0];
+titulo4.innerText = "QUARTO";
+titulo4.style.fontSize = "2rem";
