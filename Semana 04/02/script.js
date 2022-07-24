dadf6b62638dd603026c292bc99993763bfa2af9
Igor Almeida <96877821+igorAlmeida00@users.noreply.github.com>
2022-07-24 19:32:56 -0300

    Add files via upload

---
+++ Semana 04/02/script.js
@@ -0,0 +1,15 @@
+/*Crie uma classe chamada Filme com os atributos nome, 
+anoDeLancamento e diretor. Fora do escopo dessa classe, 
+instancie ela e preencha os campos acessando seus 
+atributos por meio do ponto (.).*/
+
+class movie{
+  nome;
+  anoDeLancamento;
+  diretor;
+}
+
+const filme = new movie();
+filme.nome = "Blade Runner";
+filme.anoDeLancamento = 1982;
+filme.diretor = "Ridley Scott";
